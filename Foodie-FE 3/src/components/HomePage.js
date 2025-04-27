00c6ee976f31ec9a74531370eaabaf113756cb72
Vinthiya <vinthiyaperi14@gmail.com>
2025-04-27 14:24:44 +0530

    Bug fixes 6

--- Foodie-FE 3/src/components/HomePage.js
+++ Foodie-FE 3/src/components/HomePage.js
@@ -70,7 +70,9 @@ function HomePage() {
             <Row>
               <Link to="/signin" className={styles.linkTextFormat}><Button variant="success" className={`${styles.btnHomePage} mb-3`}>Sign In <RiLoginBoxLine /></Button></Link>
             </Row>
-            
+            <Row>
+            <Link to="/signup" className={styles.linkTextFormat}><Button variant="success" className={styles.btnHomePage}>Sign Up <BsFillPersonPlusFill /></Button></Link>
+            </Row>
           </div>
         </Col>
       </Row>
